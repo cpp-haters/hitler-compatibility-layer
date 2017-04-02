f7427274d745e41bdeb4d4e590e0d2e1a468b863
wvffle <casper@wvffle.net>
2017-04-02 15:25:39 +0200

    Update hcl.hpp

--- hcl.hpp
+++ hcl.hpp
@@ -34,3 +34,13 @@
 #define loschen delete
 #define Namenraum namespace
 #define bleibend const
+#define Null 0
+#define eins 1
+#define zwei 2
+#define drei 3
+#define vier 4
+#define funf 5
+#define sechs 6
+#define sieben 7
+#define acht 8
+#define neun 9
